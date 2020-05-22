6fcd2e02390a9cfe6daaf44e4ff46e6bb4e1c0ae
s0sa <jackgrundy@fastmail.com>
2020-05-22 12:13:39 +0100

    Add email capability

--- GIMPS_LL_notify.sh
+++ GIMPS_LL_notify.sh
@@ -1,21 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 # Check the prime.log every hour and send an email with any new results
 
-#TODO check for log file presence
-#TODO wait until on the hour to email
+if test -f "prime.log" ; then
+    old_result_count=$(grep -c "prime" prime.log)
+    sleep_time=1h
 
-old_result_count=$(grep -c "prime" prime.log)
-sleep_time=1h
+    while true; do
+        current_result_count=$(grep -c "prime" prime.log)
+        if [[ $current_result_count > $old_result_count ]] ; then
+            new_result=$(grep "prime" prime.log | tail -1)
+            # send mail
+            {
+                echo From: your_email@domain.com
+                echo Subject: GIMPS LL Test Result
+                echo "$new_result"
+            } | ssmtp recipient_email@domain.com
+            old_result_count=$current_result_count
+            sleep $sleep_time
+        else
+            sleep $sleep_time
+        fi
+    done
 
-while true; do
-    current_result_count=$(grep -c "prime" prime.log)
-    if [[ $current_result_count > $old_result_count ]] ; then
-        get the result line
-        mail the updated results
-        old_result_count=$current_result_count
-        sleep $sleep_time
     else
-        sleep $sleep_time
-    fi
-done
+        echo "prime.log not found in this directory"
+fi
